fe90f99e2247f29cdd437d9843161109e6a78648
Fahad <fahad630321@gmail.com>
2025-12-13 13:00:25 +0530

    Clean up code in Skeleton component

--- src/components/ui/Skeleton.jsx
+++ src/components/ui/Skeleton.jsx
@@ -1,4 +1,3 @@
-// Generic Skeleton Block
 export const Skeleton = ({ className = "", ...props }) => {
   return (
     <div
@@ -8,14 +7,11 @@ export const Skeleton = ({ className = "", ...props }) => {
   );
 };
 
-// Skeleton for a single movie card
 export const MovieCardSkeleton = () => {
   return (
     <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-      {/* Poster */}
       <Skeleton className="w-full h-64" />
 
-      {/* Movie info */}
       <div className="p-3">
         <Skeleton className="h-4 w-3/4 mb-2" />
         <Skeleton className="h-3 w-1/2" />
@@ -24,7 +20,6 @@ export const MovieCardSkeleton = () => {
   );
 };
 
-// Grid of movie card skeletons
 export const GridSkeleton = ({ count = 20 }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
